ca345293e06ea130bbb2dd03525ca27c429733d0
shinymalar <shinybp@gmail.com>
2019-09-26 13:37:43 +0530

    i have done my coding

---
+++ src/org/test/task2.java
@@ -0,0 +1,17 @@
+package org.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class task2 {
+	public static void main(String[] args) throws Exception {
+	System.setProperty("webdriver.chrome.driver", "C:\\eclipse-committers-oxygen-3a-win32-x86_64\\eclipse\\shiny\\SeleniumTask\\driver\\chromedriver.exe");
+	WebDriver d=new ChromeDriver();
+	d.get("https://www.flipkart.com/");
+	WebElement cls = d.findElement(By.xpath("//button[@class='_2AkmmA _29YdH8']"));
+	cls.click();
+
+}
+}
